26f9581dc6ba8a5b530b6631eb55ef6923f5a6db
Vignesh-Kumar-07 <71544868+Vignesh-Kumar-07@users.noreply.github.com>
2024-08-02 10:59:47 +0530

    done

--- src/ui/Poster.jsx
+++ src/ui/Poster.jsx
@@ -7,7 +7,14 @@ function Poster() {
 
   return (
     <div className="poster">
-      <img src={`https://test.create.diagnal.com/images/${poster}`} alt="" />
+      <img
+        src={`https://test.create.diagnal.com/images/${
+          poster === "posterthatismissing.jpg"
+            ? "placeholder_for_missing_posters.png"
+            : poster
+        }`}
+        alt=""
+      />
       {/* <button  className ='btn' onClick={() => navigate(-1)}>back</button> */}
       <AiTwotoneCloseCircle className="btn" onClick={() => navigate(-1)} />
     </div>
