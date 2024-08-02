1e6e61a228ffe2ac9dfa2f33293ba44b5cfe09e9
Vignesh-Kumar-07 <71544868+Vignesh-Kumar-07@users.noreply.github.com>
2024-08-02 09:49:18 +0530

    fixed

--- src/ui/SearchMovie.jsx
+++ src/ui/SearchMovie.jsx
@@ -1,30 +1,14 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 import { useMovie } from "../context/MovieContext";
 import { CiSearch } from "react-icons/ci";
 function SearchMovie() {
   const { searchQuery, setSearchQuery } = useMovie();
-  const [enter, setEnter] = useState(true);
 
   return (
     <div className="search">
       <div className="searchContainer">
         <img src="https://test.create.diagnal.com/images/Back.png" alt="" />
 
-        {/* {enter ? (
-          <img
-            src="https://test.create.diagnal.com/images/search.png"
-            alt=""
-            onMouseEnter={() => setEnter(false)}
-          />
-        ) : (
-          <input
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search posts..."
-            onMouseLeave={() => setEnter(true)}
-          />
-        )} */}
         <div className="box">
           <input
             type="text"
